66211008eea8a06e537f2dddfe719148e9158414
ar00ster18 <arielarusy12@gmail.com>
2024-11-25 22:25:47 +0200

    Made some improvements

--- Algebra.java
+++ Algebra.java
@@ -78,9 +78,16 @@ public class Algebra {
 	// Returns the integer part of x1 / x2 
 	public static int div(int x1, int x2) {
 		int quotient = 0;
-		while(x1 >= x2){
-			x1 = minus(x1, x2);
-			quotient++;
+		if (x1 > 0){
+			while(x1 >= x2){
+				x1 = minus(x1, x2);
+				quotient++;
+			}
+		} else {
+			while(x1 <= x2){
+				x1 = plus(x1, x2);
+				quotient++;
+			}
 		}
 		return quotient;
 	}
