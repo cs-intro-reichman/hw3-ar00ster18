a9a240620aa713b7b46f337497fe0971a07eea1b
ar00ster18 <arielarusy12@gmail.com>
2024-11-26 17:51:57 +0200

    the last push did not work

--- LoanCalc.java
+++ LoanCalc.java
@@ -57,10 +57,11 @@ public class LoanCalc {
 	// the number of periods (n), and epsilon, the approximation's accuracy
 	// Side effect: modifies the class variable iterationCounter.
     public static double bisectionSolver(double loan, double rate, int n, double epsilon) {  
-		iterationCounter = 0;
+		iterationCounter = -1;
         double L = loan/n;
 		double H = 2*loan;
 		double g = (L + H)/2;
+		
 		while(H - L > epsilon){
 			if(endBalance(loan, rate, n, g)*endBalance(loan, rate, n, L) > 0){
 				L = g;
